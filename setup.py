6b05e784ab06f4072a0a80055c86d2aa9f299a54
Sergey Pikhovkin <pikhovkin@gmail.com>
2016-12-14 00:38:52 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name='null_object',
+      version='0.1',
+      description='Null object',
+      author='Sergey Pikhovkin',
+      author_email='s@pikhovkin.ru',
+      url='https://github.com/pikhovkin/null_object',
+      packages=['null_object'],
+      include_package_data=False,
+      install_requires=[],
+      dependency_links=[])
